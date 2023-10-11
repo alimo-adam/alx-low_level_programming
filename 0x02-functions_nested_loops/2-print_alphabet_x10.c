17670bf8776e84377f1d8ccd4cf7c9536d011df8
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-10-11 18:36:12 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char letter;
+int i;
+char letter;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-            _putchar(letter);
+for (i = 0; i < 10; i++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
         
-        _putchar('\n');
-    }
+_putchar('\n');
+}
 }
