f92aa2d8c7560c779f1cf06c71800847251ca2ef
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-10-11 18:12:22 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Print "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     printf("_putchar\n");
