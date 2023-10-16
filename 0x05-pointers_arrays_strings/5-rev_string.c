d442f62e4861c6d7ba13d140a3088d220dd99bc7
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-10-16 17:41:41 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,26 @@
 #include "main.h"
 
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be reversed
+ *
+ * Return: void
+ */
 void rev_string(char *s)
 {
-    int len = 0, i = 0;
+    int len = 0;
+    int i = 0;
     char tmp;
 
+    /* Calculate the length of the string */
     while (s[len] != '\0')
     {
         len++;
     }
 
-    len -= 1; /* Adjust length to exclude '\0' */
+    len -= 1; /* Adjust length for zero indexing */
 
+    /* Swap characters for the first half of the string with the second half */
     while (i < len)
     {
         tmp = s[i];
