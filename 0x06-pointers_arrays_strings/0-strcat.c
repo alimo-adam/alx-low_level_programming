384a6903b645b9b8bc9fa03b568c445db692ea51
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-10-20 16:16:19 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,23 +7,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = 0;
-	int i = 0;
+int dest_len = 0;
+int i = 0;
 
-	/* Calculate the length of dest */
-	while (dest[dest_len] != '\0')
-		dest_len++;
+/* Calculate the length of dest */
+while (dest[dest_len] != '\0')
+dest_len++;
 
-	/* Append src to dest */
-	while (src[i] != '\0')
-	{
-		dest[dest_len] = src[i];
-		i++;
-		dest_len++;
-	}
+/* Append src to dest */
+while (src[i] != '\0')
+{
+dest[dest_len] = src[i];
+i++;
+dest_len++;
+}
 
-	/* Add null terminator at the end */
-	dest[dest_len] = '\0';
+/* Add null terminator at the end */
+dest[dest_len] = '\0';
 
-	return (dest);
+return (dest);
 }
