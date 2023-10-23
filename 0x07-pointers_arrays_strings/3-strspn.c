c8fa92a669beb21f57254fcbec9c2289ec0aebf8
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-10-23 18:13:47 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,33 +1,35 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: initial segment of the string to calculate the length of
- * @accept: string containing the list of characters to match in s
- * Return: number of bytes in the initial segment of s which consist
- * only of bytes from accept
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to scan
+ * @accept: the string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment of s which
+ * consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j, count;
+    unsigned int bytes = 0;
+    char *ptr;
 
-    count = 0;
-    for (i = 0; s[i] != '\0'; i++)
+    while (*s && *accept)
     {
-        for (j = 0; accept[j] != '\0'; j++)
+        for (ptr = accept; *ptr; ptr++)
         {
-            if (s[i] == accept[j])
+            if (*s == *ptr)
             {
-                count++;
+                bytes++;
                 break;
             }
         }
 
-        if (accept[j] == '\0')
-        {
-            return count; // return count if no more matches
-        }
+        // If no match found, break out of the loop
+        if (!(*ptr))
+            break;
+
+        s++;
     }
 
-    return count;
+    return bytes;
 }
