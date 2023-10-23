212d315824682ea85409e4395d0c88166fe30879
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-10-23 18:11:36 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,35 +2,20 @@
 
 /**
  * _strchr - locates a character in a string
- * @s: the string to search
- * @c: the character to locate
- *
- * Return: pointer to the first occurrence of the character
- * c in the string s, or NULL if the character is not found
+ * @s: the string to be searched
+ * @c: the character to look for
+ * 
+ * Return: pointer to the first occurrence of the character c in s,
+ * or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-    /* Validate input */
-    if (s == NULL)
+    while (*s)
     {
-        return NULL;
+        if (*s == c)
+            return s;
+        s++;
     }
 
-    /* Loop through each character of string */
-    while (*s != '\0')  /* Check end of string */
-    {
-        if (*s == c)  /* If character is found */
-        {
-            return s;  /* Return pointer to current position */
-        }
-        s++;  /* Move to next character */
-    }
-
-    /* Handle case when character is '\0' */
-    if (c == '\0')
-    {
-        return s;
-    }
-
-    return NULL;  /* If character not found */
+    return (*s == c) ? s : NULL;
 }
