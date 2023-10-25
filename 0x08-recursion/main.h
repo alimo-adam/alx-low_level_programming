6c17474b3484841878a3f981de7793c301548f70
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-10-25 11:03:55 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -24,4 +24,7 @@ int _pow_recursion(int x, int y);
 /* Function to return the natural square root of a number */
 int _sqrt_recursion(int n);
 
+/* Function to check if n is a prime number */
+int is_prime_number(int n);
+
 #endif /* MAIN_H */
