723d82f52ed904252305746e6e04c65390f507be
Ali Adam <alihasham7860@gmail.com>
2023-10-25 00:15:21 -0700

    add codes

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+/* Function to print character */
+int _putchar(char c);
+
+/* Function to print string using recursion */
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
+
