b33d598d2a14069b1e3c1b7212f9c7a607614e8f
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-10-25 10:35:22 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strlen_recursion - Returns the length of a string using recursion.
  * @s: The string.
@@ -8,10 +7,9 @@
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        return (0);
-    }
-    return (1 + _strlen_recursion(s + 1));
+if (*s == '\0')
+{
+return (0);
 }
-
+return (1 + _strlen_recursion(s + 1));
+}
