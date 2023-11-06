eddf8235572ec31575f837ce8863dd8f3dcc1050
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-11-06 16:15:03 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-* struct dog - a dog's basic info
-* @name: First member, the dog's name
-* @age: Second member, the dog's age
-* @owner: Third member, the dog's owner
+* struct dog - structure for a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
 */
 struct dog
 {
@@ -12,4 +12,5 @@ char *name;
 float age;
 char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
