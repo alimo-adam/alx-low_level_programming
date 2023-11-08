80ed20b7cf58592bb39498df4af326b38e7c37f4
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-11-08 14:43:48 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
- * get_op_func - Selects the correct function to perform the operation
- * @s: The operator passed as argument to the program
- * Return: A pointer to the function that corresponds to the operator
+ * get_op_func - selects the correct function to perform the operation
+ * @s: the operator passed as an argument
+ *
+ * Return: a pointer to the function that corresponds to the operator
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -17,17 +16,15 @@ int (*get_op_func(char *s))(int, int)
         {"%", op_mod},
         {NULL, NULL}
     };
-
     int i = 0;
 
     while (ops[i].op)
     {
-        if (ops[i].op[0] == s[0] && s[1] == '\0')
+        if (*ops[i].op == *s && s[1] == '\0')
+        {
             return (ops[i].f);
+        }
         i++;
     }
-
-    printf("Error\n");
-    exit(99);
+    return (NULL);
 }
-
