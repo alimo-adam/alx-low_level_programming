506552b0dbba253c52f7865eef79db7a38cc9a78
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-11-08 14:18:51 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 /**
  * print_name - prints a name using a function pointer
  * @name: The name to be printed
- * @f: The function pointer to a printing function
+ * @f: A function pointer to determine how to print the name
  */
 void print_name(char *name, void (*f)(char *))
 {
