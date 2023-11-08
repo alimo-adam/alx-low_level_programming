972e14e1eda5d28fc061c8189d184155be6e57f8
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-11-08 14:51:45 +0300

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,7 +3,6 @@
 
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
@@ -13,10 +12,7 @@ typedef struct op
     int (*f)(int a, int b);
 } op_t;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+/* Function prototypes */
+int (*get_op_func(char *s))(int, int);
 
-#endif /* CALC_H */
+#endif
