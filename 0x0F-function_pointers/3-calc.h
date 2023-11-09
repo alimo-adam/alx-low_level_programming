82f4821bcb364381f345acfb5bdfc248ebccb551
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-11-09 07:24:46 +0300

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,20 +1,23 @@
-#ifndef _HEADER_
-#define _HEADER_
-/**
-* struct op - Struct ор
-*
-* @op: The operator
-* @f: The function associated
-*/
-typedef struct ор
-{
-char *ор;
-int (*f)(int а, int b);
-} op_t;
-int op_add(int а, int b);
-int op_sub(int а, int b);
-int op_mul(int а, int b);
-int op_div( int а, int b);
-int op_mod(int а, int b);
-int (*get_op_func(char *s))(int, int);
-#endif
+#ifndef CALC_H
+	#define CALC_H
+	/**
+	 * struct op - A struct op.
+	 * @op: The operator.
+	 * @f: The associated function.
+	 */
+	typedef struct op
+	{
+		char *op;
+		int (*f)(int a, int b);
+	} op_t;
+
+
+	int op_add(int a, int b);
+	int op_sub(int a, int b);
+	int op_mul(int a, int b);
+	int op_div(int a, int b);
+	int op_mod(int a, int b);
+	int (*get_op_func(char *s))(int, int);
+
+
+	#endif
