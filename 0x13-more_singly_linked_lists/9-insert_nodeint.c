3ab1ac4573b53faa8a0be70dc45dcca868a5115c
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-11-20 19:38:04 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,64 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - returns the nth node of a linked list
- * @head: pointer to the head of the list
- * @idx: index of the node to be added
- * @n: content of the new node
+ * insert_nodeint_at_index - sum the values of the list.
  *
- * Return: the address of the node
+ * @head: A pointer to the first node of the list
+ * @idx: index of the list.
+ * @n: value of number.
+ * Return: Value of a node index.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = NULL;
-	listint_t *previous_node = NULL;
-	unsigned int i = 0;
+	unsigned int i = 1;
+	listint_t *index_node = *head;
+	listint_t *insert;
+	listint_t *h = *head;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL || idx > listint_len(*head))
-	{
-		free(new_node);
+	if (head == NULL)
 		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = NULL;
-	while (head != NULL)
+	while (i < idx)
 	{
-		if (i == idx)
-		{
-			if (i == 0)
-			{
-				new_node->next = *head;
-				*head = new_node;
-				return (new_node);
-			}
-			new_node->next = previous_node->next;
-			previous_node->next = new_node;
-			return (new_node);
-		}
-		else if ((i + 1) == idx)
-			previous_node = *head;
-		head = &((*head)->next);
-		i++;
+		index_node = (*head)->next;
+		*head = index_node;
+		++i;
 	}
-	return (NULL);
-}
+	insert = malloc(sizeof(listint_t));
 
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
+	if (insert == NULL)
+		return (NULL);
+	insert->n = n;
+	if (idx == 0)
 	{
-		count += 1;
-		cursor = cursor->next;
+		insert->next = *head;
+		*head = insert;
 	}
-	return (count);
+	else
+	{
+		insert->next = (*head)->next;
+		(*head)->next = insert;
+		*head = h;
+	}
+	return (insert);
 }
