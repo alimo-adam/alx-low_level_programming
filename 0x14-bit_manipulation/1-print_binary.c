af4993720138eaf69172f0d1d196fe2c4dad8efe
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-11-23 16:06:18 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
-#include "holberton.h"
-#include <math.h>
+#include "main.h"
+
 /**
- * print_binary - Function that prints the binary representation of a number.
- * Prototype: void print_binary(unsigned long int n);
- * @n: number to convert to binary
- * You are not allowed to use arrays
- * You are not allowed to use malloc
- * You are not allowed to use the % or / operators
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+	if (n >> 0)
 	{
-		putchar('0');
-		return;
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-	else if (n == 1)
+	else
 	{
-		putchar ('1');
-		return;
+		_putchar('0');
 	}
-	print_binary(n >> 1);
-	putchar('0' + (n & 1));
 }
