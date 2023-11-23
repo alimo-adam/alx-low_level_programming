06478ab70cf23d45c1a94fc1572b49ae85137fa2
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2023-11-23 16:11:27 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,28 +1,24 @@
-#include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
- * flip_bits - Write a function that returns the number of bits you would
- * need to flip to get from one number to another.
- * Prototype: unsigned int flip_bits(unsigned long int n, unsigned long int m);
- * You are not allowed to use the % or / operators
- * @n: Number Input 1.
- * @m: Number Input 2.
- * Return: Number of flip in the Input numbers.
+ * clear_bit - sets the value of a bit to 0.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
-
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int flip = 0, comp, comp1;
+	unsigned int m;
 
-	while (!(n == 0 && m == 0))
-	{
-		comp = n & 1;
-		comp1 = m & 1;
-		n = n >> 1;
-		m = m >> 1;
-		if (comp != comp1)
-			flip += 1;
-	}
-	return (flip);
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
+	return (1);
 }
