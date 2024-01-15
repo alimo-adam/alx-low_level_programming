fe19c5237d27f5043af0301d2742e0b5fb34eac6
alimo-adam <144421365+alimo-adam@users.noreply.github.com>
2024-01-15 16:11:21 +0300

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-# Compile all .c files into position-independent code and create shared library
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c *.c
+gcc -fPIC -c *.c
 gcc -shared -o liball.so *.o
-rm *.o
